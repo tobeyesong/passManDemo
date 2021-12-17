d132b71b4b93766f2a7ab40ac21384c722456ba4
Toan Lam <toanlam01@gmail.com>
2021-12-16 22:28:55 -0800

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 /** @format */
-
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
@@ -16,9 +16,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use("/api/passwords", passwordRoutes);
 
