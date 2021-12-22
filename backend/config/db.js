c5c639a6aafa7bcf0b9e3eb888a3a6589972d790
Toan Lam <toanlam01@gmail.com>
2021-12-21 18:46:51 -0800

    update env, split DBUSER and DBPASS

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jcidy.mongodb.net/passManDemo?retryWrites=true&w=majority`,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
